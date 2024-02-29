5b463a7fc2421ea7f918f78589b20c51c412c485
yangtao01 <yangtao01@zhihu.com>
2024-02-29 14:50:30 +0800

    delete tpl

--- script.js
+++ script.js
@@ -3,20 +3,16 @@ const fs = require('fs');
 
 const pages = new Array(19).fill('1');
 
-
-
 const list = pages.reduce((acc, cur, idx) => {
   const data = require(`./jsonshanyu/data${idx+1}.json`);
   return [...acc, ...data.data.list]
 }, [])
 
 
-const originHtml = fs.readFileSync('./tpl').toString()
-
-const html = originHtml.replace('__data__', JSON.stringify(list))
+const originHtml = fs.readFileSync('./index.html').toString();
 
 
-fs.writeFileSync('./index.html', html)
+fs.writeFileSync('./index.html', originHtml.replace(/<script\sid=\"INJECT\">.+<\/script>/, `<script id="INJECT">const data = ${JSON.stringify(list)}</script>`))
 
 
 
