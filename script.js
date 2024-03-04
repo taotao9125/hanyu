8c52c4f5587ff355ccc5dee4282cb559af8429d4
yangtao01 <yangtao01@zhihu.com>
2024-03-04 11:47:56 +0800

    opti: read file name by fs

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const fs = require('fs');
 
+const fileNames = fs.readdirSync('./jsonshanyu');
 
-const pages = new Array(19).fill('1');
-
-const list = pages.reduce((acc, cur, idx) => {
+const list = fileNames.reduce((acc, cur, idx) => {
   const data = require(`./jsonshanyu/data${idx+1}.json`);
   return [...acc, ...data.data.list]
 }, [])
 
 
 const originHtml = fs.readFileSync('./index.html').toString();
-
 
 fs.writeFileSync('./index.html', originHtml.replace(/<script\sid=\"INJECT\">.+<\/script>/, `<script id="INJECT">const data = ${JSON.stringify(list)}</script>`))
 
