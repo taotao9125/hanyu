e08c4d4f755ec67deef16021bd4374f0b91c25ba
yangtao01 <yangtao01@zhihu.com>
2024-05-07 12:15:40 +0800

    重新组织数据

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 
-const fileNames = fs.readdirSync('./jsonshanyu');
+const w = fs.readFileSync('./wordlist.txt', 'utf-8');
 
-const list = fileNames.reduce((acc, cur, idx) => {
-  const data = require(`./jsonshanyu/data${idx+1}.json`);
-  return [...acc, ...data.data.list]
-}, [])
+const list = w
+  .split('\n').map(el => el.split(/\t+/g))
+  .filter(el => el.every(el => !!el))
+  .filter(el => el.length === 4)
+  .map(el => {
+    const [py, def] = el[2].split('##')
+    return {
+      definition: def,
+      sid: el[0],
+      sugPy: py[0],
+      name: el[1],
+      pinyin: py,
+      time: el[3]
+    }
+  });
+
+
 
 
 const originHtml = fs.readFileSync('./index.html').toString();
